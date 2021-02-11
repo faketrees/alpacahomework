9aacf790bd83aa8f34812e95edce9ca0fbd6eff2
Michael Lyons <michaellyons@Michaels-MBP.bishopdynamics.com>
2021-02-11 00:50:47 -0800

    Cookies/rspec homework

---
+++ w4d3/Cookies/spec/dessert_spec.rb
@@ -0,0 +1,76 @@
+require 'rspec'
+require 'dessert'
+
+=begin
+Instructions: implement all of the pending specs (the `it` statements without blocks)! Be sure to look over the solutions when you're done.
+=end
+
+describe Dessert do
+  let(:chef) { double("chef", name: "Timmy") }
+  let(:cookie) { Dessert.new("cookie", 75, chef) }  
+
+  describe "#initialize" do
+    it "sets a type" do
+      expect(cookie.type).to eq("cookie")
+    end
+
+    it "sets a quantity" do
+      expect(cookie.quantity).to eq(75)
+    end
+
+    it "starts ingredients as an empty array" do
+      expect(cookie.ingredients).to be_empty
+    end
+
+    it "raises an argument error when given a non-integer quantity" do
+      expect {Dessert.new("brownie", "two", chef)}.to raise_error(ArgumentError)
+    end
+  end
+
+  describe "#add_ingredient" do
+    it "adds an ingredient to the ingredients array" do
+      cookie.add_ingredient("chocolate chips")
+      expect(cookie.ingredients).to include("chocolate chips")
+    end
+  end
+
+  describe "#mix!" do
+    it "shuffles the ingredient array" do
+      ingredients = ["milk", "flour", "sugar", "chocolate", "eggs", "butter", "water", "oil"]
+
+      ingredients.each do |ingredient|
+        cookie.add_ingredient(ingredient)
+      end
+
+      expect(cookie.ingredients).to eq(ingredients)
+      cookie.mix!
+      expect(cookie.ingredients).not_to eq(ingredients)
+      expect(cookie.ingredients.sort).to eq(ingredients.sort)
+    end
+  end
+
+  describe "#eat" do
+    it "subtracts an amount from the quantity" do
+      cookie.eat(25)
+      expect(cookie.quantity).to eq(50)
+    end
+
+    it "raises an error if the amount is greater than the quantity" do
+      expect { cookie.eat(100)}.to raise_error("not enough left!")
+    end
+  end
+
+  describe "#serve" do
+    it "contains the titleized version of the chef's name" do
+      allow(chef).to receive(:titleize).and_return("Chef Timmy the Great Baker")
+      expect(cookie.serve).to eq("Chef Timmy the Great Baker has made 75 cookies!")
+    end
+  end
+
+  describe "#make_more" do
+    it "calls bake on the dessert's chef with the dessert passed in" do
+      expect(chef).to receive(:bake).with(cookie)
+      cookie.make_more
+    end
+  end
+end
